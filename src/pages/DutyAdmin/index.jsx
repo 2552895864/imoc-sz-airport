8ad8c262fc49f53f17f0b2feaf14ae09c87edfc9
吴鑫博 <wuxbl@digitalchina.com>
2020-07-12 15:55:53 +0800

    添加模块文件夹

---
+++ src/pages/DutyAdmin/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class DutyAdmin extends React.Component {
+  render() {
+    return <div>DutyAdmin</div>;
+  }
+}
---
+++ src/pages/ResourceState/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class ResourceState extends React.Component {
+  render() {
+    return <div>ResourceState</div>;
+  }
+}
