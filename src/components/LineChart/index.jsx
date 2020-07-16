0d7dc6c901462c666d088f429a97d06dba04b819
肖子立 <xiaozlb@digitalchina.com>
2020-07-16 11:58:55 +0800

    调整曲线图组件、应用态势曲线图示例

--- src/components/LineChart/index.jsx
+++ src/components/LineChart/index.jsx
@@ -9,13 +9,24 @@ import styles from "./index.module.less";
  * @param {className} 类名（需指定宽高） e.g. .chart { width: 432px !important; height: 187px !important; }
  * @param {data} 数据对象
  * @param {options} echarts配置对象（除去数据相关配置）
+ * @param {labelClass} x轴label类名 e.g. .label { bottom: 4.81% !important; left: 6.94% !important; font-size: 12px !important; }
  * @param {label} x轴名称
  */
 
-const LineChart = ({ className, data, options, label = "日期" }) => {
+const LineChart = ({
+  className,
+  data,
+  options,
+  labelClass,
+  label = "日期",
+}) => {
   const ChartClass = classnames({
     [styles.chart]: true,
     [className]: className,
+  });
+  const LabelClass = classnames({
+    [styles.xlabel]: true,
+    [labelClass]: labelClass,
   });
   return (
     <div className={styles.container}>
@@ -23,7 +34,7 @@ const LineChart = ({ className, data, options, label = "日期" }) => {
         className={ChartClass}
         chartOption={getChartsOption(options, data)}
       ></Echarts>
-      <div className={styles.xlabel}>{label}</div>
+      <div className={LabelClass}>{label}</div>
     </div>
   );
 };
