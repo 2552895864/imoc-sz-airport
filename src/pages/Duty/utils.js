a7c0b016d8abd3dc1b9d8197f7c3fd27083f3fb3
肖子立 <xiaozlb@digitalchina.com>
2020-07-16 09:50:32 +0800

    新文案效果

--- src/pages/Duty/utils.js
+++ src/pages/Duty/utils.js
@@ -1,8 +1,8 @@
 const generatePhoneCardString = (staff) => {
-  const shiftMapping = { D: "（白）", N: "（晚）" };
+  const shiftMapping = { D: "白：", N: "晚：" };
   const { staffName, staffMobile, shift } = staff;
   const shiftString = !shift ? "" : shiftMapping[shift];
-  return `${staffName}${shiftString} ${staffMobile}`;
+  return `${shiftString}${staffName} ${staffMobile}`;
 };
 
 export { generatePhoneCardString };
